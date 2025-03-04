8dabbd8845ed4ecaa83d7baee569bdeecd331a86
jbwfu <75001777+jbwfu@users.noreply.github.com>
2025-03-04 18:45:08 +0800

    feat(last.fm): scrobble point

--- internal/lastfm/track.go
+++ internal/lastfm/track.go
@@ -16,10 +16,11 @@ type Tracker struct {
 	client *Client
 	l      sync.Mutex
 
-	enable     bool                  // 是否启用，功能不可用时为本地模式
-	pending    *storage.ScrobbleList // 待上报项
-	nowPlaying storage.Scrobble      // 当前播放
-	runonce    chan bool             // 用于触发一次执行
+	enable        bool                  // 是否启用，功能不可用时为本地模式
+	scrobblePoint int                   // 可 Scrobble 百分比
+	pending       *storage.ScrobbleList // 待上报项
+	nowPlaying    storage.Scrobble      // 当前播放
+	runonce       chan bool             // 用于触发一次执行
 }
 
 func NewTracker(client *Client, ctx context.Context) *Tracker {
@@ -29,6 +30,7 @@ func NewTracker(client *Client, ctx context.Context) *Tracker {
 		pending: &storage.ScrobbleList{},
 		runonce: make(chan bool, 1),
 	}
+	t.setScrobblePoint(configs.ConfigRegistry.Lastfm.ScrobblePoint)
 	t.pending.InitFromStorage()
 
 	// Scrobble 监听
@@ -126,6 +128,14 @@ func (t *Tracker) Playing(scrobble storage.Scrobble) {
 	}
 }
 
+func (t *Tracker) setScrobblePoint(point int) {
+	if point < 50 || point > 100 {
+		slog.Error("ScrobblePoint 大小须为 50~100，使用默认值 50")
+		point = 50
+	}
+	t.scrobblePoint = point
+}
+
 func (t *Tracker) Status() bool {
 	return t.enable
 }
@@ -175,7 +185,10 @@ func (t *Tracker) IsScrobbleable(duration, played float64) bool {
 	if played >= 4*60 { // 大于 4min 直接上报
 		return true
 	}
-	return played >= duration/2 // 过半
+	if t.scrobblePoint == 100 && duration-played < 3 { // 近似完整播放
+		return true
+	}
+	return played >= duration*float64(t.scrobblePoint/100) // 自定义上报起始比例
 }
 
 const scrobbleExpiryDays = 14
